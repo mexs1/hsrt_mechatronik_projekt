608b913aac2b3ec23ee435c2535fb567c3e389d2
max <max>
2016-10-20 00:00:53 +0200

    2.

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+/*
+Software zur Anbindung des Shunts zur Messung der Ströme
+Autor: Maximilian Serfas
+Datum: 19.10.2016
+*/
+
 #include <stdio.h>
 #include <ADuCM360.H>
 
 int main (void) {
+	
 	SystemCoreClockUpdate();
 }
