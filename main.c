64a65d1300f817c03522b824f373e15bac6a7f64
Maximilian Serfas <max_serfas@gmx.net>
2016-12-21 19:02:34 +0100

    Main.h hinzugefügt; main.c geändert. ->Wert muss stimmen, allerdings nicht nachvollziehbar bei debug

--- main.c
+++ main.c
@@ -6,12 +6,14 @@ Datum: 19.10.2016
 
 #include <stdio.h>
 #include <ADuCM360.H>
+#include "main.h"
 
 int main (void) {
 	
 	//Variablen
 	double resistance = 0.0001; 
-	int analog_value = 0;
+	double analog_value = 0;
+	int temp_value = 0;
 	double current = 0;
 	
 	SystemCoreClockUpdate();
@@ -20,10 +22,16 @@ int main (void) {
 	pADI_ADC0->CON |= ADCCON_ADCEN;	      //Aktiviere ADC0
 	pADI_ADC0->CON |= ADCCON_ADCCP_AIN0;  //Positiver Kanal : AIN0
 	pADI_ADC0->CON |= ADCCON_ADCCN_AIN5;  //Negativer Kanal : AIN5
-	pADI_ADC0->MDE |= ADCMDE_ADCMD_CONT;  //Kontinuierliche Messung über ADC
+	
+	pADI_ADC0->MDE |= ADCMDE_ADCMD_IDLE;    //Idle-Mode zur Kalibrierung
+	pADI_ADC0->MDE |= ADCMDE_ADCMD_INTOCAL; //Zero-Scale-Calibration
+	pADI_ADC0->MDE = ADCMDE_ADCMD_CONT;    //Kontinuierliche Messung über
 	
 	while(1){
-  	analog_value = pADI_ADC0->DAT;
+  	temp_value = pADI_ADC0->DAT;
+		//analog_value = ADC_FACTOR * analog_value;
+		analog_value = 0.1966953277 * (double)temp_value;
+		analog_value = analog_value/1000000;
 		current = analog_value/resistance;
 	}
 }
---
+++ main.h
@@ -0,0 +1,10 @@
+/*
+Header-Datei für Konstanten
+*/
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define ADC_FACTOR = 0.1966953277; //Faktor zwecks Umrechnung aus Datenregister zur tatsächlichen Spannung
+
+#endif //MAIN_H
